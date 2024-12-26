f0a1972cccf7b5c56b0ca0e59fe8e4574dbedc05
Ilya Paymushkin <ipaymushkin76@gmail.com>
2024-12-26 18:02:53 +0300

    сделал создание слотов

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import AdminPage from "../pages/admin";
 import { routes } from "../config/routes.ts";
 import AdminEdit from "../pages/adminEdit";
+import { useEffect } from "react";
 
 const Pages = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(routes.admin.path);
+  }, []);
+
   return (
     <Routes>
       <Route path={routes.admin.path} element={<AdminPage />} />
